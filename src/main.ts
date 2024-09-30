2777e80cbeb7054c84311bb8f6fd4d0a6850e1d6
pendulum <rs.pendulum@gmail.com>
2024-09-30 16:19:57 +0300

    added config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { CONFIG_APP } from './config/config.export';
 
 
 async function bootstrap() {
@@ -10,7 +11,7 @@ async function bootstrap() {
       transport: Transport.KAFKA,
       options: {
         client: {
-          brokers: ['localhost:9092'],
+          brokers: [CONFIG_APP.KAFKA_BROKER],
         }
       },
     },
